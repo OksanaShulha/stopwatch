d135a3904720912b4cd003cde73ac45c0996447b
OksanaShulha <shushullja@gmail.com>
2021-08-04 18:37:14 +0200

    Add data

---
+++ data.js
@@ -0,0 +1,17 @@
+export const state = {
+  interval: undefined,
+  timerInterval: undefined,
+  counter: 0,
+  time: {
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+    milliseconds: 0,
+  },
+  lapList: [],
+  setTimer: {
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  },
+};
