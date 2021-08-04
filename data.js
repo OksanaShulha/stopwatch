2fc4128553964de29f74d825a90d49d63095f144
Oksana Shulha <shushullja@gmail.com>
2021-08-04 18:41:41 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,17 +1,8 @@
 export const state = {
-  interval: undefined,
-  timerInterval: undefined,
-  counter: 0,
   time: {
     hours: 0,
     minutes: 0,
     seconds: 0,
     milliseconds: 0,
-  },
-  lapList: [],
-  setTimer: {
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
   },
 };
